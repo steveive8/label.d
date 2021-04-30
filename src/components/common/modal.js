955f304e93f63875bfeac2bb8fa020e62f75209c
johnsteve108 <buddhychoi@gmail.com>
2021-04-30 18:20:59 +0900

    commit

---
+++ src/components/common/modal.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import {Row, Col, Flex, Text, Image} from '../base';
+
+export const Modal = ({}) => {
+    return (
+        <Row>
+            <Flex>
+                <Image />
+            </Flex>
+            <Col>
+                
+            </Col>
+        </Row>
+    )
+};
+
+export default Modal;
